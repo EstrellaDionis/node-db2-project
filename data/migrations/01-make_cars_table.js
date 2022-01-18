24d5cfe15d10ad35bec09249e78490900e27daaa
EstrellaDionis <EstrellaDionis@gmail.com>
2022-01-18 18:37:09 -0500

    created cars in seeds and exports.up function in make_cars_table.js

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,21 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', table => {
+    table.increments() //leaving this empty because i.d is the default
+
+    table.string('vin', 17).notNullable().unique()
+
+    table.string('make', 128).notNullable()
+
+    table.string('model', 128).notNullable()
+
+    table.numeric('mileage').unsigned().notNullable() //unsigned is to make it so mileage is not a negative number
+
+    table.string('title', 128)
+    
+    table.string('transmission', 128)
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
